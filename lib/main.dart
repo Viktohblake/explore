c48749d72536743e3e37d9aca9dcad0c2000ef64
viktohblake <damanygt@gmail.com>
2022-11-09 17:06:42 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:explore/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Explore',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
